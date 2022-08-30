09e406a6283763ee03f6312b1ba0a44f01e3648a
Konstantin Pivnov <vaproloff@ya.ru>
2022-08-30 23:33:30 +0300

    added task3 solution

---
+++ seminar1-task3/Program.cs
@@ -0,0 +1,49 @@
+﻿//=======================================================
+// # 3 Напишите программу, которая будет выдавать название
+// дня недели по заданному номеру.
+//=======================================================
+
+string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+    int inputDayOfWeek = int.Parse(inputLine);
+
+    // Вариант решения 1
+    // string[] dayOfWeek = new string[7];
+    // dayOfWeek[0] = "Понедельник";
+    // dayOfWeek[1] = "Вторник";
+    // dayOfWeek[2] = "Среда";
+    // dayOfWeek[3] = "Четверг";
+    // dayOfWeek[4] = "Пятница";
+    // dayOfWeek[5] = "Суббота";
+    // dayOfWeek[6] = "Воскресенье";
+
+    // if (inputDayOfWeek > 7 || inputDayOfWeek < 1)
+    // {
+    //     Console.WriteLine("Такого дня нет");
+    // }
+    // else
+    // {
+    //     Console.WriteLine(dayOfWeek[inputDayOfWeek - 1]);
+    // }
+    // ==============================================
+
+    // Вариант решения2
+    string outDayOfWeek = string.Empty; // ""
+
+    switch (inputDayOfWeek)
+    {
+        case 1: outDayOfWeek = "Понедельник"; break;
+        case 2: outDayOfWeek = "Вторник"; break;
+        case 3: outDayOfWeek = "Среда"; break;
+        case 4: outDayOfWeek = "Четверг"; break;
+        case 5: outDayOfWeek = "Пятница"; break;
+        case 6: outDayOfWeek = "Суббота"; break;
+        case 7: outDayOfWeek = "Воскресенье"; break;
+        default: outDayOfWeek = "Такого дня нет"; break;
+    }
+
+    Console.WriteLine(outDayOfWeek);
+    // ==============================================
+}
