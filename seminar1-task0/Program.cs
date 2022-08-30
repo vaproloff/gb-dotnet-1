cb3447112457b58a9ba5424ab931005de4e18268
Konstantin Pivnov <vaproloff@ya.ru>
2022-08-30 23:25:46 +0300

    added task0 solution

--- seminar1-task0/Program.cs
+++ seminar1-task0/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//=======================================================
+// # 0 Напишите программу, которая на вход принимает
+// число и выдаёт его квадрат.
+//=======================================================
+
+string? inputLine = Console.ReadLine();
+if (inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
+    
+    // int outNumber = 0;
+    // DateTime d1 = DateTime.Now;
+    // for (int i = 0; i < 10000000; i++)
+    // {
+    //     outNumber = (int)Math.Pow(inputNumber, 2);
+    // }
+    // DateTime d2 = DateTime.Now;
+    // Console.WriteLine(d2 - d1);
+    
+    int outNumber = inputNumber * inputNumber;
+
+    Console.WriteLine(outNumber);
+}
