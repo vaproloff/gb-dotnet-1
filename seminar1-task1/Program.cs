6364025ece0ef8396d6509fed31174378ec494a0
Konstantin Pivnov <vaproloff@ya.ru>
2022-08-30 23:29:22 +0300

    added task1 solution

---
+++ seminar1-task1/Program.cs
@@ -0,0 +1,19 @@
+﻿//=======================================================
+// # 1 Напишите программу, которая на вход принимает два числа
+// и проверяет, является ли первое число квадратов второго.
+//=======================================================
+
+string? inputLineA = Console.ReadLine();
+string? inputLineB = Console.ReadLine();
+
+if (inputLineA != null && inputLineB != null)
+{
+    int inputNumberA = int.Parse(inputLineA);
+    int inputNumberB = int.Parse(inputLineB);
+
+    // bool outResult = (inputNumberA * inputNumberA == inputNumberB); // Вариант 1
+    // bool outResult = (inputNumberB / inputNumberA == inputNumberA); // Вариант 2
+    bool outResult = (Math.Sqrt(inputNumberB) == inputNumberA); // Вариант 3
+
+    Console.WriteLine(outResult);
+}
