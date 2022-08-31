68a0dbbc246ecb8cd480e01fd2817ce63ee88b3a
Konstantin Pivnov <vaproloff@ya.ru>
2022-08-31 15:46:25 +0300

    added task2 alternative solution

--- seminar1-task2/Program.cs
+++ seminar1-task2/Program.cs
@@ -13,6 +13,24 @@ if (inputLineA != null && inputLineB != null)
     int inputNumberA = int.Parse(inputLineA);
     int inputNumberB = int.Parse(inputLineB);
 
+    // Вариант решения 1
+    // string answerMax = "";
+
+    // if (inputNumberA == inputNumberB)
+    // {
+    //     answerMax = "Два числа равны между собой";
+    // }
+    // else
+    // {
+    //     string maxNumberA = "Число " + inputNumberA + " больше, чем число " + inputNumberB;
+    //     string maxNumberB = "Число " + inputNumberB + " больше, чем число " + inputNumberA;
+    //     answerMax = inputNumberA > inputNumberB ? maxNumberA : maxNumberB;
+    // }
+
+    // Console.WriteLine(answerMax);
+    // =================
+
+    // Вариант решения 2
     if (inputNumberA > inputNumberB)
     {
         Console.WriteLine("Число " + inputNumberA + " больше, чем число " + inputNumberB);
@@ -25,4 +43,5 @@ if (inputLineA != null && inputLineB != null)
     {
         Console.WriteLine("Два числа равны между собой");
     }
+    // =================
 }
