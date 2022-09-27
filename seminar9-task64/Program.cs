096b487bfcdaee7bdae39db6e4ee112236ee6398
Konstantin Pivnov <vaproloff@ya.ru>
2022-09-27 22:21:04 +0300

    added task 64 solution

---
+++ seminar9-task64/Program.cs
@@ -0,0 +1,32 @@
+﻿//=======================================================
+// # 64 Напишите программу, которая выдаёт все натуральные
+// числа в промежутке от N до 1
+//=======================================================
+
+// Метод считывания данных пользователя
+int ReadData(string line)
+{
+    // Выводим сообщение
+    Console.Write(line);
+    // Считываем число
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    // Возвращаем значение
+    return number;
+}
+
+// Метод, принимает строку, выводит в консоль
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+// Метод, число N, возвращает строку чисел от N до 1
+string LineGenRec(int numN)
+{
+    if (numN == 0) return ""; //Точка остановки
+    string outline = numN + " " + LineGenRec(numN - 1);
+    return outline;
+}
+
+int inputNum = ReadData("Введите число: ");
+PrintResult(LineGenRec(inputNum));
